a2f2ac9d71fd0aa5141dab0cb4bc5a69d817fe52
ice <meltedise@gmail.com>
2012-02-12 04:06:12 +0900

    added month and mday for age calculation.

--- lib/date_age.rb
+++ lib/date_age.rb
@@ -1,7 +1,20 @@
-require "date_age/version"
+require 'date'
+require 'date_age/version'
 
 class Date
   def age_at(date)
-    date.year - self.year
+    birthday = self
+    today    = date
+    age = today.year - birthday.year
+    if today.month < birthday.month
+      age -= 1
+    elsif birthday.month == today.month
+      age -= 1 if today.day < birthday.day
+    end
+    age
+  end
+
+  def age
+    age_at(Date.today)
   end
 end
--- spec/date_age_spec.rb
+++ spec/date_age_spec.rb
@@ -6,24 +6,33 @@ describe DateAge do
   def birthday(str) Date.parse(str) end
   def today(str)    Date.parse(str) end
 
-  describe "age_at between different years" do
-    it "should be 1 year old" do
-      birthday("2010/01/01").age_at(today("2011/01/01")).should == 1
-      birthday("2010/01/01").age_at(today("2011/05/05")).should == 1
-      birthday("2010/01/01").age_at(today("2011/12/31")).should == 1
+  describe "age_at" do
+    describe "between different years" do
+      it "should be 1 year old" do
+        birthday("2010/01/01").age_at(today("2011/01/01")).should == 1
+        birthday("2010/01/01").age_at(today("2011/05/05")).should == 1
+        birthday("2010/01/01").age_at(today("2011/12/31")).should == 1
+      end
+
+      it "should be greater than 1 year old" do
+        birthday("1980/01/01").age_at(today("2011/01/01")).should == 31
+        birthday("1980/01/01").age_at(today("2011/05/05")).should == 31
+        birthday("1980/01/01").age_at(today("2011/12/31")).should == 31
+      end
     end
 
-    it "should be greater than 1 year old" do
-      birthday("1980/01/01").age_at(today("2011/01/01")).should == 31
-      birthday("1980/01/01").age_at(today("2011/05/05")).should == 31
-      birthday("1980/01/01").age_at(today("2011/12/31")).should == 31
+    describe "between the same years" do
+      it "should be 0 years old" do
+        birthday("2010/01/01").age_at(today("2010/01/01")).should == 0
+        birthday("2010/01/01").age_at(today("2010/12/31")).should == 0
+      end
     end
-  end
 
-  describe "age_at between the same years" do
-    it "should be 0 years old" do
-      birthday("2010/01/01").age_at(today("2010/01/01")).should == 0
-      birthday("2010/01/01").age_at(today("2010/12/31")).should == 0
+    describe "0 years old between different years" do
+      it "should be 0 years old" do
+        birthday("2010/02/01").age_at(today("2011/01/31")).should == 0
+        birthday("2010/02/02").age_at(today("2011/02/01")).should == 0
+      end
     end
   end
 end
