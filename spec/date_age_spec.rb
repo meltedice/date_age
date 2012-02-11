434f302226bfdbdfb91ca5c51acdf79603ae939f
ice <meltedise@gmail.com>
2012-02-12 03:28:13 +0900

    added more tests for simple calculation with years.

--- spec/date_age_spec.rb
+++ spec/date_age_spec.rb
@@ -3,29 +3,27 @@
 require "spec_helper"
 
 describe DateAge do
-  describe "age_at" do
-    it "should be 0 years old" do
-      birthday = Date.parse("2010/01/01")
-      today    = Date.parse("2010/01/01")
-      birthday.age_at(today).should == 0
+  def birthday(str) Date.parse(str) end
+  def today(str)    Date.parse(str) end
 
-      birthday = Date.parse("2010/01/01")
-      today    = Date.parse("2010/12/31")
-      birthday.age_at(today).should == 0
+  describe "age_at between different years" do
+    it "should be 1 year old" do
+      birthday("2010/01/01").age_at(today("2011/01/01")).should == 1
+      birthday("2010/01/01").age_at(today("2011/05/05")).should == 1
+      birthday("2010/01/01").age_at(today("2011/12/31")).should == 1
     end
 
-    it "should be 1 year old" do
-      birthday = Date.parse("2010/01/01")
-      today    = Date.parse("2011/01/01")
-      birthday.age_at(today).should == 1
+    it "should be greater than 1 year old" do
+      birthday("1980/01/01").age_at(today("2011/01/01")).should == 31
+      birthday("1980/01/01").age_at(today("2011/05/05")).should == 31
+      birthday("1980/01/01").age_at(today("2011/12/31")).should == 31
+    end
+  end
 
-      birthday = Date.parse("2010/01/01")
-      today    = Date.parse("2011/05/05")
-      birthday.age_at(today).should == 1
-
-      birthday = Date.parse("2010/01/01")
-      today    = Date.parse("2011/12/31")
-      birthday.age_at(today).should == 1
+  describe "age_at between the same years" do
+    it "should be 0 years old" do
+      birthday("2010/01/01").age_at(today("2010/01/01")).should == 0
+      birthday("2010/01/01").age_at(today("2010/12/31")).should == 0
     end
   end
 end
